b6aaed83919f1c587f92a98e1f4a90acab5da67c
Kate Dameron <katerj33@gmail.com>
2019-02-25 13:51:00 -0800

    App

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
+import ColorSelector from './ColorSelector';
+import TextSelector from './TextSelector';
+import Shape from './Shape';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-
+    this.eleRef = React.createRef();
   }
 
   state = {
-    text: '',
-    background: ''
+    text: 'hello',
+    backgroundColor: 'red',
+    fontColor: 'black'
   }
 
-  // handleChange = ({ target }) => {
-  //   event.preventDefault();
-  //   this.setState({ [target.text]: target.value })
-  // }
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
 
   render() {
     return(
-      <h1>hi</h1>
+      <>
+      <TextSelector />
+      <ColorSelector 
+      onChange={this.handleChange} 
+      background={this.state.backgroundColor} fontColor={this.state.fontColor}/>
+      <Shape 
+      eleRef={this.eleRef}
+      text={this.state.text}
+      backgroundColr={this.state.backgroundColor}
+      fontColor={this.fontcolor}
+      />
+      </>
     )
   }
 
