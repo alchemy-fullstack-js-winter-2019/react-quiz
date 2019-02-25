fe32e71844f929bb59e9b6a75b0f18081fb27c4b
Tyler Corbett <thetylercorbett@gmail.com>
2019-02-25 13:13:07 -0800

    hello world app rendering

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+
+class App extends PureComponent {
+    
+  render(){
+    return (
+      <h1>Hi</h1>
+    );
+  }
+    
+}
+
+
+export default App;
