e51789baa60224fd8bb62d98dd2b96b7ff415cbb
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-02-25 13:35:18 -0800

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 import ColorSelector from './ColorSelector';
 import TextSelector from './TextSelector';
+import Shape from './Shape';
 
 
 class App extends Component {
     state = {
         FontColor: 'black',
-        BackgroundColor: 'white'
-    }
+        BackgroundColor: 'white',
+        ImageText: ''
+    };
 
     handleChange = ({ target }) => {
         this.setState({ [target.name]: target.value });
     };
 
     render() {
+        const { FontColor, BackgroundColor, ImageText } = this.state;
         return (
         <>
-      <h1> Hello world</h1>
-      <ColorSelector handleChange={this.handleChange}/>
-      <TextSelector handleChange={this.handleChange}/>
-
-      </>
+            <h1> Hello world</h1>
+            <ColorSelector handleChange={this.handleChange}/>
+            <TextSelector handleChange={this.handleChange}/>
+            <Shape
+                FontColor={FontColor}
+                BackgroundColor={BackgroundColor}
+                ImageText={ImageText}
+            />
+        </>
         );
     }
 }
